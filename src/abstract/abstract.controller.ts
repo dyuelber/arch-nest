ec53290d9dd182df1fc21aa7a52acf54d8567757
Dyuelber Rodrigues <dyuelbermiranda@outlook.com>
2023-04-18 11:01:34 -0300

    fixes

--- src/abstract/abstract.controller.ts
+++ src/abstract/abstract.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AbstractService } from './abstract.service';
 import { ObjectId } from 'mongoose';
 
@@ -7,12 +7,18 @@ export class AbstractController {
   constructor(protected service: AbstractService) {}
 
   @Get()
-  async find(@Param('filters') filters: string) {
+  async find(@Param('filters') filters) {
+    // erro
     return this.service.find(filters);
   }
 
   @Get(':id')
   async findById(@Param('id') id: string | ObjectId) {
     return this.service.findById(id);
+  }
+
+  @Post()
+  async create(@Body() params) {
+    return this.service.create(params);
   }
 }
