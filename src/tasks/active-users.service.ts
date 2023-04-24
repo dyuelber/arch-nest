d1fa09e305da2a2e04155e76b7812bf8fb000794
Dyuelber Rodrigues <dyuelbermiranda@outlook.com>
2023-04-24 16:41:29 -0300

    implement example inject services in tasks service

--- src/tasks/active-users.service.ts
+++ src/tasks/active-users.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
 import { ScheduleInterface } from './schedule.interface';
+import { UsersService } from '../users/users.service';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class ActiveUsersService implements ScheduleInterface {
-  @Cron(CronExpression.EVERY_SECOND)
-  run(): void {
-    console.log('active users');
+  constructor(protected service: UsersService) {}
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async run() {
+    //const user = await this.service.findById('643ea10c9753f2277588f91f');
+    console.log('users');
   }
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DisableUsersService } from './disable-users.service';
 import { ActiveUsersService } from './active-users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Users, UsersSchema } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]),
+  ],
   controllers: [],
-  //providers: [DisableUsersService, ActiveUsersService],
+  providers: [UsersService, DisableUsersService, ActiveUsersService],
 })
 export class TasksModule {}
