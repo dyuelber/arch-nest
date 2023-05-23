3a974b42875c6f3c8c1341df3acc9ac6fb5652e4
Dyuelber Rodrigues <dyuelbermiranda@outlook.com>
2023-05-23 09:40:31 -0300

    comment abstract controller

--- src/abstract/abstract.controller.ts
+++ src/abstract/abstract.controller.ts
@@ -16,77 +16,76 @@ import { RequestsPipe } from '../validations/requests.pipe';
 import { AbstractInterface, ControllerOptions } from './abstract.interface';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-export function abstract(options: ControllerOptions) {
-  abstract class Abstract implements AbstractInterface {
-    constructor(public service: AbstractService) {}
+// export function abstract(options: ControllerOptions) {
+//   abstract class Abstract implements AbstractInterface {
+//     constructor(public service: AbstractService) {}
 
-    @Get()
-    @UseGuards(AuthGuard)
-    async find(@Query('filters') filters: any): Promise<any[]> {
-      return this.service.find(filters);
-    }
+//     @Get()
+//     @UseGuards(AuthGuard)
+//     async find(@Query('filters') filters: any): Promise<any[]> {
+//       return this.service.find(filters);
+//     }
 
-    @Post()
-    @ApiOperation(options.apiCreateSchema)
-    @ApiResponse(options.apiCreateResponseSchema)
-    @UseGuards(AuthGuard)
-    @UsePipes(new RequestsPipe(options.createValidation))
-    async create(@Body() params: any): Promise<any> {
-      console.table(params);
-      try {
-        this.service.begin();
-        const response = await this.service.create(params);
-        await this.service.commit();
+//     @Post()
+//     @ApiOperation(options.apiCreateSchema)
+//     @ApiResponse(options.apiCreateResponseSchema)
+//     @UseGuards(AuthGuard)
+//     @UsePipes(new RequestsPipe(options.createValidation))
+//     async create(@Body() params: any): Promise<any> {
+//       try {
+//         this.service.begin();
+//         const response = await this.service.create(params);
+//         await this.service.commit();
 
-        return response;
-      } catch (error) {
-        await this.service.rollback();
-      }
-    }
+//         return response;
+//       } catch (error) {
+//         await this.service.rollback();
+//       }
+//     }
 
-    @Get(':id')
-    @UseGuards(AuthGuard)
-    async findById(@Param('id') id: string | ObjectId): Promise<any> {
-      return this.service.findById(id);
-    }
+//     @Get(':id')
+//     @UseGuards(AuthGuard)
+//     async findById(@Param('id') id: string | ObjectId): Promise<any> {
+//       return this.service.findById(id);
+//     }
 
-    @Put(':id')
-    @ApiOperation(options.apiUpdateSchema)
-    @ApiResponse(options.apiUpdateResponseSchema)
-    @UseGuards(AuthGuard)
-    @UsePipes(new RequestsPipe(options.updateValidation))
-    async update(
-      @Param('id') id: string | ObjectId,
-      @Body() params: any,
-    ): Promise<any> {
-      try {
-        this.service.begin();
-        const response = await this.service.update(id, params);
-        await this.service.commit();
+//     @Put(':id')
+//     @ApiOperation(options.apiUpdateSchema)
+//     @ApiResponse(options.apiUpdateResponseSchema)
+//     @UseGuards(AuthGuard)
+//     @UsePipes(new RequestsPipe(options.updateValidation))
+//     async update(
+//       @Param('id') id: string | ObjectId,
+//       @Body() params: any,
+//     ): Promise<any> {
+//       try {
+//         this.service.begin();
+//         const response = await this.service.update(id, params);
+//         await this.service.commit();
 
-        return response;
-      } catch (error) {
-        await this.service.rollback();
-      }
-    }
+//         return response;
+//       } catch (error) {
+//         await this.service.rollback();
+//       }
+//     }
 
-    @Delete(':id')
-    @UseGuards(AuthGuard)
-    async delete(@Param('id') id: string | ObjectId): Promise<any> {
-      try {
-        this.service.begin();
-        const response = this.service.delete(id);
-        await this.service.commit();
+//     @Delete(':id')
+//     @UseGuards(AuthGuard)
+//     async delete(@Param('id') id: string | ObjectId): Promise<any> {
+//       try {
+//         this.service.begin();
+//         const response = this.service.delete(id);
+//         await this.service.commit();
 
-        return response;
-      } catch (error) {
-        await this.service.rollback();
-      }
-    }
-  }
+//         return response;
+//       } catch (error) {
+//         await this.service.rollback();
+//       }
+//     }
+//   }
 
-  return Abstract;
-}
+//   return Abstract;
+// }
 
 // export abstract class AbstractController {
 //   protected createValidation: Joi.ObjectSchema<any>;
